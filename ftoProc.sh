141d83ded903ca74f6785ef0bf6707ce079fdc9a
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-02-27 14:38:51 -0800

    bx-gitRepos-auto

--- ftoProc.sh
+++ ftoProc.sh
@@ -13,13 +13,13 @@ __copying__="
 
 ####+BEGIN: bx:bsip:bash:seed-spec :types "seedFtoCommon.sh"
 SEED="
-*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/git/auth/bxRepos/bisos/ftoProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/bisos/ftoProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
-    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
+    /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@"
     exit $?
 fi
 ####+END:
@@ -57,15 +57,18 @@ _CommentEnd_
 ####+BEGIN: bx:dblock:ploneProc:bash:nodesList :types ""
 # {{{ DBLOCK-nodesList
 nodesList="
-overview
-bxso
-bpip1
-comeega
-bxio
-gatherer
-bsip4
-defaults
+dev
+mail
 asc
+bsip4
+pals
+defaults
+gatherer
+overview
+apps
+comeega
+bpip1
+lcnt
 "
 # }}} DBLOCK-nodesList
 ####+END:
